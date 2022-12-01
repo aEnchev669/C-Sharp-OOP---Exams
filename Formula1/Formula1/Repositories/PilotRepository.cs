33e26b778d4de28ccc15feeb1b49552780e2f227
aEnchev669 <aenchev669@abv.bg>
2022-12-01 16:08:03 +0200

    Initial commit

---
+++ Formula1/Formula1/Repositories/PilotRepository.cs
@@ -0,0 +1,34 @@
+﻿using Formula1.Models.Contracts;
+using Formula1.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Formula1.Repositories
+{
+    public class PilotRepository : IRepository<IPilot>
+    {
+        public PilotRepository()
+        {
+            models = new List<IPilot>();
+        }
+        private readonly List<IPilot> models;
+        public IReadOnlyCollection<IPilot> Models => this.models.AsReadOnly();
+
+        public void Add(IPilot model)
+        {
+            models.Add(model);
+        }
+
+        public IPilot FindByName(string name)
+        {
+            return models.FirstOrDefault(p => p.FullName == name);
+        }
+
+        public bool Remove(IPilot model)
+        {
+            return models.Remove(model);
+        }
+    }
+}
