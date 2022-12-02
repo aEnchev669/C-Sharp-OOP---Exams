44bd3e5deb65982f549cb58323a22eb81198b8c6
aEnchev669 <aenchev669@abv.bg>
2022-12-02 22:25:48 +0200

    Exam

---
+++ BookSkeleton/Book.Tests/Tests.cs
@@ -0,0 +1,33 @@
+﻿namespace Book.Tests
+{
+    using System;
+
+    using NUnit.Framework;
+
+    public class Tests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+        [Test]
+        public void ConstructorShouldInitializeBookNameCorrectly()
+        {
+            string expectedBookName = "Harry Potter";
+            Book book = new Book(expectedBookName, "J. K. Rowling");
+
+            string actualBookName = book.BookName;
+            Assert.AreEqual(expectedBookName, actualBookName);
+        }
+        [Test]
+        public void ConstructerShloudInitializeAuthorName()
+        {
+            string expectedAuthor = "J. K. Rowling";
+            Book book = new Book("Harry Potter", expectedAuthor);
+
+            string actualAuthorName = book.BookName;
+            Assert.AreEqual(expectedAuthor, actualAuthorName);
+        }
+    }
+}
