6f3ca530501e81f99a2ea1755c8b1ea00c54eea2
aEnchev669 <aenchev669@abv.bg>
2022-12-09 03:09:57 +0200

    Exam preparation

---
+++ C# OOP Exam - 16 August 2020/OnlineShop-Skeleton/OnlineShop/Models/Products/Product.cs
@@ -0,0 +1,99 @@
+﻿using OnlineShop.Common.Constants;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineShop.Models.Products
+{
+    public abstract class Product : IProduct
+    {
+        public Product(int id, string manufacturer, string model, decimal price, double overallPerformance)
+        {
+            Id = id;
+            Manufacturer = manufacturer;
+            Model = model;
+            Price = price;
+            OverallPerformance = overallPerformance;
+        }
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidProductId);
+                }
+                id = value;
+            }
+
+        }
+
+
+        private string manufacturer;
+
+        public string Manufacturer
+        {
+            get { return manufacturer; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidManufacturer);
+                }
+                manufacturer = value;
+            }
+        }
+        private string model;
+
+        public string Model
+        {
+            get { return model; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidModel);
+                }
+                model = value;
+            }
+        }
+        private decimal price;
+
+        public decimal Price
+        {
+            get { return price; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidPrice);
+                }
+                price = value;
+            }
+        }
+        private double OverallPerformanco;
+
+        public double OverallPerformance
+        {
+            get { return OverallPerformanco; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidOverallPerformance);
+                }
+                OverallPerformanco = value;
+            }
+        }
+
+
+
+        public override string ToString()
+        {
+            return $"Overall Performance: {OverallPerformance}. Price: {Price} - {GetType().Name}: {Manufacturer} {Model} (Id: {Id})";
+        }
+    }
+}
