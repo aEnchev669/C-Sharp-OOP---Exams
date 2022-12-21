9cb3a2ca5773c053c608e8f168e03ad85e3884b1
aEnchev669 <aenchev669@abv.bg>
2022-12-21 12:36:08 +0200

    Exam

---
+++ !!!!!!!!!!!!!!!!!!!!!!!!/Models/Subject.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UniversityCompetition.Models.Contracts;
+using UniversityCompetition.Utilities.Messages;
+
+namespace UniversityCompetition.Models
+{
+    public abstract class Subject : ISubject
+    {
+        public Subject(int subjectId, string subjectName, double subjectRate)
+        {
+            Id = subjectId;
+            Name = subjectName;
+            Rate = subjectRate;
+        }
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            private set { id = value; }
+        }
+
+        //  public int Id { get; private set; } //!!!!!!!!!!!!!!
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.NameNullOrWhitespace);
+                }
+                name = value;
+            }
+        }
+
+
+        private double rate;
+
+        public double Rate
+        {
+            get { return rate; }
+            private set { rate = value; }
+        }
+
+        //  public double Rate { get; private set; } //!!!!!
+    }
+}
